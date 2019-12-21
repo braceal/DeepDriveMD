bec8b30de245762dc89b50e9a2a4e01b7ea8994e
braceal <braceal@umich.edu>
2019-12-21 01:57:16 -0500

    added molecules dependency to setup

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
+# TODO: add python3 radical.entk dependency
+
 setup(
     name='deepdrive',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'molecules>=0.0.3.2',
         'pycodestyle==2.5.0',
         'pydocstyle==4.0.1',
         'pylint==2.3.1',
         'pytest==5.1.2',
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
     ],
 )
