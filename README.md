aca555e873084e774117bc12831ba6c762dfcb5f
braceal <braceal@umich.edu>
2019-12-22 21:36:25 -0500

    changed venv to conda env to incorporate openmm

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ git clone https://github.com/braceal/DeepDriveMD.git
 cd DeepDriveMD/
 
 module load python/3.7.0
-python3 -m venv env
-source env/bin/activate
-pip install --upgrade pip setuptools wheel
-pip install -e .
+conda env create python=3.7 -p ./conda-env -f environment.yml
+conda activate ./conda-env
+conda install -p ./conda-env -c omnia openmm-setup
 ```
--- setup.py
+++ setup.py
@@ -7,14 +7,6 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'molecules>=0.0.3.2',
-        'click==7.0',
-        'pycodestyle==2.5.0',
-        'pydocstyle==4.0.1',
-        'pylint==2.3.1',
-        'pytest==5.1.2',
-    ],
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
