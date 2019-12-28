28855c58ff8aa84b89f42de79cb706fc528a1cf7
braceal <braceal@umich.edu>
2019-12-28 02:59:48 -0500

    added script to take a directory with contact map h5 files from MD simulations and convert them to a single contact matrix file to be input into a machine learning model. may have bug generating the full cm matrices

--- examples/cvae_dbscan/scripts/contact_map.py
+++ examples/cvae_dbscan/scripts/contact_map.py
@@ -4,33 +4,38 @@ import click
 import warnings
 from glob import glob
 from contextlib import ExitStack
-from deepdrive.preproc import cm_to_cvae, open_h5
+from molecules.utils import open_h5
+from deepdrive.preproc import cm_to_cvae
 
-
-def validate_sim_path(ctx, param, value):
+# TODO: Make validator file. This is used in ./md.py as well
+def validate_path(ctx, param, value):
     """
     Adds abspath to non-None file
     """
     if value:
-        return os.path.abspath(value)
-    else:
-        warnings.warn('No input dirname given, using current directory...')
-        return os.path.abspath('.')
+        path = os.path.abspath(value)
+        if not os.path.exists(path):
+            raise click.BadParameter(f'path does not exist {path}')
+        return path
 
 
 @click.command()
-@click.option('-f', '--sim_path', default=None,
-              callback=validate_sim_path,
-              help='Input: OpenMM simulation path')
-@click.option('-o', '--out_path', default=None,
-              help='Output: CVAE 2D contact map h5 input file')
-def main(sim_path, out_path):
-    # TODO: generalize omm*/*_cm.h5
-    cm_filepath = os.path.join(sim_path, 'omm*/*_cm.h5')
+@click.option('-i', '--sim_path', default=None,
+              callback=validate_path,
+              help='OpenMM simulation path containing output-cm-*.h5 files')
+@click.option('-o', '--out', default=None,
+              callback=validate_path,
+              help='2D contact map h5 file')
+def main(sim_path, out):
 
-    cm_files = sorted(glob(cm_filepath)) 
+    # Define wildcard path to contact matrix data
+    cm_filepath = os.path.join(sim_path, 'output-cm-*.h5')
+
+    # Collect contact matrix file names sorted by sim_id
+    cm_files = sorted(glob(cm_filepath), 
+                      key=lambda path: int(path.split('.h5')[-2].split('-')[-1]))
     if not cm_files: 
-        raise IOError(f'No h5 file found, recheck your input filepath {sim_path}')
+        raise FileNotFoundError(f'No h5 files found, recheck your input path {sim_path}')
 
     with ExitStack() as stack:
         # Open all h5 files and add them to exit stack
@@ -41,10 +46,10 @@ def main(sim_path, out_path):
         cm_data = list(map(lambda file: file['contact_maps'], open_cm_files))
         
         # Compress all .h5 files into one in cvae format 
-        cvae_input = cm_to_cvae(cm_data_lists)
+        cvae_input = cm_to_cvae(cm_data)
 
         # Create .h5 as cvae input
-        cvae_input_file = 'cvae_input.h5'
+        cvae_input_file = os.path.join(out, 'cvae-input.h5')
 
         # Create and open contact map aggregation output file
         cvae_input_file = stack.enter_context(h5py.File(cvae_input_file, 'w'))
