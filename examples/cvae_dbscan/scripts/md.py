627e4836bac564c89001b45fb0e5963ce919fd53
braceal <braceal@umich.edu>
2019-12-28 02:12:14 -0500

    updated validators for md script arguments. added sim_id argument to give a unique integer assignment

--- examples/cvae_dbscan/scripts/md.py
+++ examples/cvae_dbscan/scripts/md.py
@@ -11,19 +11,30 @@ def validate_file(ctx, param, value):
     Adds abspath to non-None file
     """
     if value:
-        return os.path.abspath(value)
+        path = os.path.abspath(value)
+        if not os.path.exist(path)
+            raise click.BadParameter(f'path does not exist {path}')
+        return path
+
 
 def validate_len(ctx, param, value):
     """
     Checks that length is greater than 0
     """
     if value < 0:
-        raise click.BadParameter(f'must be greater than 0, currently {value}')
+        raise click.BadParameter(f'must be greater than or equal to 0, currently {value}')
+    return value
 
 
 @click.command()
 @click.option('-p', '--pdb', required=True,
               callback=validate_file, help='PDB file')
+@click.option('-o', '--out', required=True,
+              callback=validate_file,
+              help='Output directory for MD simulation data')
+@click.option('-i', '--sim_id', required=True, type=int,
+              callback=validate_len,
+              help='Simulation ID in pipeline [0...N]')
 @click.option('-t', '--topol', default=None, 
               callback=validate_file, help='Topology file')
 @click.option('-c', '--chk', default=None,
@@ -32,19 +43,19 @@ def validate_len(ctx, param, value):
 @click.option('-l', '--len', 'length', default=10, type=float,
               callback=validate_len,
               help='How long (ns) the system will be simulated')
-@click.option('-r', '--report', default=50, type=int,
+@click.option('-r', '--report', default=50, type=float,
               callback=validate_len,
               help= 'Time interval (ps) between reports')
 @click.option('-g', '--gpu', default=0, type=int, 
               help='ID of gpu to use for the simulation')
-def main(pdb, topol, chk, length, report, gpu):
-    
+def main(pdb, out, sim_id, topol, chk, length, report, gpu):
     openmm_simulate_amber_fs_pep(pdb,
-                                 check_point = chk,
+                                 checkpnt=chk,
                                  GPU_index=gpu,
-                                 output_traj='output.dcd',
-                                 output_log='output.log',
-                                 output_cm='output_cm.h5',
+                                 checkpnt_fname=os.path.join(out, f'checkpnt-{sim_id}.chk'),
+                                 output_traj=os.path.join(out, f'output-{sim_id}.dcd'),
+                                 output_log=os.path.join(out, f'output-{sim_id}.log'),
+                                 output_cm=os.path.join(out, f'output-cm-{sim_id}.h5'),
                                  report_time=report * u.picoseconds,
                                  sim_time=length * u.nanoseconds)
 
