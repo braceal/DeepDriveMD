9aa75a8f59137c3678ee203ca068711283ecb41f
braceal <braceal@umich.edu>
2020-02-06 20:48:19 -0500

    uncommented imports for python3 radical.entk

--- deepdrive/__init__.py
+++ deepdrive/__init__.py
@@ -1,3 +1,2 @@
-# TODO: uncomment once radical.entk is supported for python3
-#from deepdrive.deepdrive import DeepDriveMD
-#from deepdrive.taskman import TaskMan
+from deepdrive.deepdrive import DeepDriveMD
+from deepdrive.taskmanager import TaskManager
