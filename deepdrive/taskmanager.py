f80c937dc6ad45ad50910fb201948ae0c86fc2bd
braceal <braceal@umich.edu>
2020-03-08 20:28:50 -0400

    added functions to taskmanager to assign hardware, load environment and set the python exe. Also made a cwd member variable

--- deepdrive/taskmanager.py
+++ deepdrive/taskmanager.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
 class TaskManager(metaclass=ABCMeta):
-    def __init__(self, cpu_reqs, gpu_reqs):
+    def __init__(self, cpu_reqs, gpu_reqs, cwd):
         """
         Parameters
         ----------
@@ -11,9 +11,51 @@ class TaskManager(metaclass=ABCMeta):
         gpu_reqs : dict
             contains gpu hardware requirments for task
 
+        cwd : str
+            path from root to /DeepDriveMD directory
+
         """
         self.cpu_reqs = cpu_reqs
         self.gpu_reqs = gpu_reqs
+        self.cwd = cwd
+
+    def assign_hardware(self, task):
+        """
+        Assign GPU and CPU resources to task.
+
+        Parameters
+        ----------
+        task : radical.entk.Task
+        """
+        if self.cpu_reqs:
+            task.cpu_reqs = self.cpu_reqs
+        if self.gpu_reqs:
+            task.gpu_reqs = self.gpu_reqs
+
+    def load_environment(self, task):
+        """
+        Loads modules and activates conda environment for
+        DeepDriveMD script dependencies.
+
+        Parameters
+        ----------
+        task : radical.entk.Task
+        """
+        task.pre_exec.extend(['module load python/3.6.6-anaconda3-5.3.0',
+                              'module load hdf5/1.10.3',
+                              'module load cuda/10.1.168',
+                              '. /sw/summit/python/3.6/anaconda3/5.3.0/etc/profile.d/conda.sh',
+                              f'conda activate {self.cwd}/conda-env/'])
+
+    def set_python_executable(self, task):
+        """
+        Set task executable to python.
+
+        Parameters
+        ----------
+        task : radical.entk.Task
+        """
+        task.executable = [f'{self.cwd}/conda-env/bin/python']
 
     @abstractmethod
     def tasks(self, pipeline_id):
