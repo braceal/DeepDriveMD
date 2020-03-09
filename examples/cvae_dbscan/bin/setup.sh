20809623c1f8c3f7279734296818e1296254ff85
braceal <braceal@umich.edu>
2020-03-08 20:32:44 -0400

    added bash script to set environment variables for radical entk

--- examples/cvae_dbscan/bin/setup.sh
+++ examples/cvae_dbscan/bin/setup.sh
@@ -2,9 +2,12 @@
 
 set -Eeuo pipefail
 
-export RMQ_HOSTNAME=two.radical-project.org
-export RMQ_PORT=33235
+export RMQ_HOSTNAME=two.radical-project.org # localhost
+export RMQ_PORT=33235 # 5672
 export RADICAL_PILOT_DBURL=mongodb://hyperrct:h1p3rrc7@two.radical-project.org:27017/hyperrct
 export RADICAL_PILOT_PROFILE=True
 export RADICAL_ENTK_PROFILE=True
 
+# For debugging
+export RADICAL_LOG_TGT=radical.log
+export RADICAL_LOG_LVL=DEBUG
